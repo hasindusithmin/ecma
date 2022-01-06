ebfddf777ad54234145483d39875c8182eeb6ad0
hasindusithmin <hasindusithmin64@gmail.com>
2022-01-07 02:12:46 +0530

    i will add /add route

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ const port = 3000
 
 //GetMapping
 app.get('/',(req,res)=>{
-    res.sendStatus()
+    res.sendStatus(200)
+})
+
+//GetMapping add two number http://127.0.0.1/add/2/6
+app.get('/add/:num1/:num2',(req,res)=>{
+    const {num1,num2} = req.params
+    const ans = num1 + num2;
+    res.send(ans)
 })
 
 //called app.listen method
